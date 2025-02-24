1e39ce3813ebfb63c8f40595737a5061d36f6c0a
arashi <kairbektamerlan@gmail.com>
2025-02-24 21:14:14 +0500

    refactor

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const session = require("express-session");
 const path = require("path");
-const Laptop = require("./models/Laptop");
-const Order = require("./models/order");
+const Laptop = require("../models/Laptop");
+const Order = require("../models/Order");
 
 const router = express.Router();
 
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Order = require("../models/order");
+const Order = require("../models/Order");
 const auth = require("../middleware/auth");
 
 const router = express.Router();
